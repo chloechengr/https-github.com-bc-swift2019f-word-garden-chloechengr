4c07906b86891ae4bd6ae6b0f635bface87f2dd9
chloechengr <chengsn@bc.edu>
2019-09-15 21:29:59 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Chloe Cheng on 9/15/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
